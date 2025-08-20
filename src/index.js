74c0ef8e5eebe166b45ee4e4ce65c2a38182b6dc
ILYA <106765406+D15ND@users.noreply.github.com>
2025-08-20 03:00:47 +0300

    feat: add solution with some errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const newMatrix = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (typeof matrix[i] !== 'object') {
+      newMatrix.push(matrix[i]);
+    } else if (typeof matrix[i] === 'object') {
+      for (let k = 0; k < matrix[i]; k += 1) {
+        newMatrix.push(matrix[i][k]);
+      }
+    }
+  }
+  return newMatrix.sort((a, b) => a - b);
 };
